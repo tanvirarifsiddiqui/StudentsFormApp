ff686401456a3d12d2454d5329a5f0b435c1f7a2
tanvirarifsiddiqui <tanvirtmm@gmail.com>
2024-04-02 12:30:34 +0600

    Add project files.

---
+++ AddStudentForm.Designer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StudentsFormApp
+{
+    partial class AddStudentForm : Form
+    {
+        // Other code...
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Add code here to handle the Save button click event
+            // Validate the input fields
+            if (ValidateInput())
+            {
+                // Create a new Student object with the input data
+                Student newStudent = new Student
+                {
+                    Name = textBoxName.Text,
+                    Class = comboBoxClass.Text,
+                    DOB = dateTimePickerDOB.Value,
+                    Gender = radioButtonMale.Checked ? "Male" : "Female"
+                };
+
+                try
+                {
+                    // Insert the new student data into the database using Entity Framework
+                    using (var context = new YourDbContext()) // Replace YourDbContext with your actual DbContext class name
+                    {
+                        context.Students.Add(newStudent);
+                        context.SaveChanges();
+                    }
+
+                    // Show a success message to the user
+                    MessageBox.Show("Student information saved successfully!");
+
+                    // Close the current form and return to the main form
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Display an error message if insertion fails
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            // Perform validation for each input field
+            if (string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                MessageBox.Show("Please enter a name.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(comboBoxClass.Text))
+            {
+                MessageBox.Show("Please select a class.");
+                return false;
+            }
+
+            // Add more validation as needed
+
+            // If all validation passes, return true
+            return true;
+        }
+    }
+}
+
+
+namespace StudentsFormApp
+{
+    partial class AddStudentForm : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelClass = new System.Windows.Forms.Label();
+            this.comboBoxClass = new System.Windows.Forms.ComboBox();
+            this.labelDOB = new System.Windows.Forms.Label();
+            this.dateTimePickerDOB = new System.Windows.Forms.DateTimePicker();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(50, 50);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(150, 50);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // labelClass
+            // 
+            this.labelClass.AutoSize = true;
+            this.labelClass.Location = new System.Drawing.Point(50, 100);
+            this.labelClass.Name = "labelClass";
+            this.labelClass.Size = new System.Drawing.Size(32, 13);
+            this.labelClass.TabIndex = 2;
+            this.labelClass.Text = "Class";
+            // 
+            // comboBoxClass
+            // 
+            this.comboBoxClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxClass.FormattingEnabled = true;
+            this.comboBoxClass.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten"});
+            this.comboBoxClass.Location = new System.Drawing.Point(150, 100);
+            this.comboBoxClass.Name = "comboBoxClass";
+            this.comboBoxClass.Size = new System.Drawing.Size(200, 21);
+            this.comboBoxClass.TabIndex = 3;
+            // 
+            // labelDOB
+            // 
+            this.labelDOB.AutoSize = true;
+            this.labelDOB.Location = new System.Drawing.Point(50, 150);
+            this.labelDOB.Name = "labelDOB";
+            this.labelDOB.Size = new System.Drawing.Size(30, 13);
+            this.labelDOB.TabIndex = 4;
+            this.labelDOB.Text = "DOB";
+            // 
+            // dateTimePickerDOB
+            // 
+            this.dateTimePickerDOB.Location = new System.Drawing.Point(150, 150);
+            this.dateTimePickerDOB.Name = "dateTimePickerDOB";
+            this.dateTimePickerDOB.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDOB.TabIndex = 5;
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Location = new System.Drawing.Point(50, 200);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(42, 13);
+            this.labelGender.TabIndex = 6;
+            this.labelGender.Text = "Gender";
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(150, 200);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(48, 17);
+            this.radioButtonMale.TabIndex = 7;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(250, 200);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonFemale.TabIndex = 8;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(150, 250);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 9;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // AddStudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.radioButtonFemale);
+            this.Controls.Add(this.radioButtonMale);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.dateTimePickerDOB);
+            this.Controls.Add(this.labelDOB);
+            this.Controls.Add(this.comboBoxClass);
+            this.Controls.Add(this.labelClass);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Name = "AddStudentForm";
+            this.Text = "Add New Student";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelClass;
+        private System.Windows.Forms.ComboBox comboBoxClass;
+        private System.Windows.Forms.Label labelDOB;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOB;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
---
+++ StudentDetails.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace StudentsFormApp
+{
+    partial class StudentDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // button action for adding new student information
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+            // Creating an instance of the AddStudentForm form
+            AddStudentForm studentDetailsForm = new AddStudentForm();
+
+            // Show the StudentDetails form
+            studentDetailsForm.ShowDialog();
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.Blue;
+            this.labelTitle.Location = new System.Drawing.Point(300, 20);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(366, 26);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Dhaka Regency Hotel and Resort";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.BackColor = System.Drawing.Color.Green;
+            this.btnAddStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddStudent.ForeColor = System.Drawing.Color.Green;
+            this.btnAddStudent.Location = new System.Drawing.Point(400, 60);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(160, 30);
+            this.btnAddStudent.TabIndex = 1;
+            this.btnAddStudent.Text = "Add New Student Info";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click); // Wire up the event handler here
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.AllowUserToAddRows = false;
+            this.dataGridViewStudents.AllowUserToDeleteRows = false;
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(50, 110);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.Size = new System.Drawing.Size(844, 399);
+            this.dataGridViewStudents.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 626);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "Form1";
+            this.Text = "Student List";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+    }
+}
